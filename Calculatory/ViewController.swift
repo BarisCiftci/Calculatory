c6cd2e9f19536877b79e33fba2054e52c2f6bfa4
Baris Ciftci <barishciftci@gmail.com>
2022-09-03 19:33:02 +0200

    Ui buttons have been linked to the code in ViewController file.

--- Calculatory/ViewController.swift
+++ Calculatory/ViewController.swift
@@ -7,12 +7,103 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    
+    @IBOutlet weak var calculatorDisplay: UILabel!
+    
+    @IBAction func clearButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func devideButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func multiplyButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func minusButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func plusButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func equalButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func decimalButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func zeroButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func oneButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func twoButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func threeButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func fourButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func fiveButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func sixButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func sevenButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func eightButton(_ sender: Any)
+    {
+        
+    }
+    
+    @IBAction func nineButton(_ sender: Any)
+    {
+        
+    }
+    
+    
 
 
 }
